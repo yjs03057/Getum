1f130931ca14ff3f19408322df354af2b14f7671
이수인 <yjs03057@khu.ac.kr>
2020-07-17 23:38:46 +0900

    feat(#5): create create qr code activity

--- application/Android_/app/src/main/java/com/example/getum/MainActivity.java
+++ application/Android_/app/src/main/java/com/example/getum/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.getum;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
+    private Button createQRBtn;
+    private Button scanQRBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createQRBtn = (Button) findViewById(R.id.createQR);
+        scanQRBtn = (Button) findViewById(R.id.scanQR);
+
+        createQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, CreateQR.class);
+                startActivity(intent);
+            }
+        });
+
+        scanQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ScanQR.class);
+                startActivity(intent);
+            }
+        });
     }
 }
