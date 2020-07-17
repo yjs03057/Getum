805bf33fe6a134d489d456395bc235dc6f352b6a
이수인 <yjs03057@khu.ac.kr>
2020-07-18 01:27:44 +0900

    feat(#5): generate qr codes

--- application/Android_/app/src/main/java/com/example/getum/AnyOrientationCaptureActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.getum;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class AnyOrientationCaptureActivity extends CaptureActivity {
-}
--- application/Android_/app/src/main/java/com/example/getum/ScanQR.java
+++ application/Android_/app/src/main/java/com/example/getum/ScanQR.java
@@ -1,14 +1,39 @@
 package com.example.getum;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanQR extends AppCompatActivity {
-
+    private IntentIntegrator qrScan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+
+        qrScan = new IntentIntegrator(this);
+        qrScan.setOrientationLocked(false);
+        qrScan.setPrompt("QR코드/바코드를 인식시켜주세요");
+        qrScan.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                //qrcode 인식 되었을 때 Action
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
