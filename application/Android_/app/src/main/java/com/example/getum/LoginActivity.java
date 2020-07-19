ed47683a4c44a58f66287b63e893eb55ce52940b
DakyungGo <ludkg@khu.ac.kr>
2020-07-19 12:05:57 +0900

    toolbare title set style

--- application/Android_/app/src/main/java/com/example/getum/LoginActivity.java
+++ application/Android_/app/src/main/java/com/example/getum/LoginActivity.java
@@ -72,6 +72,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);//액티비티 이동
+                    finish();
                 }
             }
         });
