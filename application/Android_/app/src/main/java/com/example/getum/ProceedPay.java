deebdd5871e0521bbbc835fb8c8b7c440508acf0
이수인 <yjs03057@khu.ac.kr>
2020-07-19 17:21:25 +0900

    fix(#12): fix layouts

--- application/Android_/app/src/main/java/com/example/getum/ProceedPay.java
+++ application/Android_/app/src/main/java/com/example/getum/ProceedPay.java
@@ -1,18 +1,20 @@
 package com.example.getum;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.getum.SQLite.SQLiteDBHelper;
 
 public class ProceedPay extends AppCompatActivity {
     private ImageButton backButton;
     private Button backToMenuButton;
     private Intent beforeIntent;
+    private SQLiteDBHelper dbHelper;
     public String id;
     public String name;
     public String cardno;
@@ -23,15 +25,34 @@ public class ProceedPay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proceed_pay);
 
+        dbHelper = new SQLiteDBHelper(this);
+
         backButton = (ImageButton) findViewById(R.id.pay_back_button);
         backToMenuButton = (Button) findViewById(R.id.back_to_menu);
         beforeIntent = getIntent();
         getExtras(beforeIntent);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ProceedPay.this, MainActivity.class);
+                setExtras(intent);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        backToMenuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ProceedPay.this, MainActivity.class);
+                setExtras(intent);
+                startActivity(intent);
+                finish();
+            }
+        });
         insertToDB();
     }
 
     protected void insertToDB(){
-
     }
 
     protected void getExtras(Intent beforeIntent){
@@ -40,5 +61,12 @@ public class ProceedPay extends AppCompatActivity {
         cardno = beforeIntent.getExtras().getString("cardno");
         phoneno = beforeIntent.getExtras().getString("phoneno");
         storage_id = beforeIntent.getExtras().getString("storage_id");
+    }
+
+    protected void setExtras(Intent newIntent){
+        newIntent.putExtra("id", id);
+        newIntent.putExtra("name", name);
+        newIntent.putExtra("cardno", cardno);
+        newIntent.putExtra("phoneno", phoneno);
     }
 }
